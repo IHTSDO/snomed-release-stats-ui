29d66cf523d793abe39f02d6e2566b62ce298f2e
Peter Duff <pdu@ihtsdo.org>
2018-08-17 11:01:46 +0100

    PIP-51 Angular Template Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { ExampleServiceService } from './services/example-service.service';
 import { AppComponent } from './app.component';
 import { ExampleConceptSearchPipe } from './pipes/example-concept-search.pipe';
 import { ExamplePageComponent } from './pages/example-page/example-page.component';
-import { ExampleComponentComponent } from './components/example-component/example-component.component';
+import { SnomedNavbarComponent } from './components/snomed-navbar/snomed-navbar.component';
+import { SnomedFooterComponent } from './components/snomed-footer/snomed-footer.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         ExampleConceptSearchPipe,
         ExamplePageComponent,
-        ExampleComponentComponent
+        SnomedNavbarComponent,
+        SnomedFooterComponent
     ],
     imports: [
         BrowserModule
---
+++ src/app/components/snomed-navbar/snomed-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-snomed-navbar',
+  templateUrl: './snomed-navbar.component.html',
+  styleUrls: ['./snomed-navbar.component.scss']
+})
+export class SnomedNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
