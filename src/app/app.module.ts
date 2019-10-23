f8d180fb5d7257b8bf488a47c7bb6667e700b0f1
Peter Duff <pdu@ihtsdo.org>
2019-10-23 11:23:10 +0100

    PIP-85 Template updated with interceptors and typeahead

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,45 @@
+// FRAMEWORK IMPORTS
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ExampleServiceService } from './services/example-service.service';
 import { AppComponent } from './app.component';
-import { ExampleConceptSearchPipe } from './pipes/example-concept-search.pipe';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HeaderInterceptor } from './interceptors/header.interceptor';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+
+// COMPONENT IMPORTS
 import { SnomedNavbarComponent } from './components/snomed-navbar/snomed-navbar.component';
 import { SnomedFooterComponent } from './components/snomed-footer/snomed-footer.component';
+
+// PIPE IMPORTS
+import { ExampleConceptSearchPipe } from './pipes/example-concept-search.pipe';
+
+// SERVICE IMPORTS
+import { ExampleServiceService } from './services/example-service.service';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ExampleConceptSearchPipe,
         SnomedNavbarComponent,
-        SnomedFooterComponent
+        SnomedFooterComponent,
+        ExampleConceptSearchPipe
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        NgbTypeaheadModule
     ],
     providers: [
-        ExampleServiceService
+        ExampleServiceService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: HeaderInterceptor,
+            multi: true
+        }
     ],
     bootstrap: [AppComponent]
 })
