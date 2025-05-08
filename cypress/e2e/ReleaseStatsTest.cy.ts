c2d360637d94c826e3d28fdb9cea63f2662bda7a
Elena Ilyukhina <eil@ihtsdo.org>
2025-05-08 18:12:35 +0100

    BROWSE-742 Fix Cypress tests

--- cypress/e2e/ReleaseStatsTest.cy.ts
+++ cypress/e2e/ReleaseStatsTest.cy.ts
@@ -1,15 +1,14 @@
-import {it} from "mocha"
-import {StartPage} from "../pages/StartPage"
-import { QaStatPage } from "../pages/QaStatsPage"
+import StartPage from "../pages/StartPage";
+import ReleaseStatsPage from "../pages/ReleaseStatsPage";
 
-const startPage = new StartPage()
-const qaStatPage = new QaStatPage()
+const startPage = new StartPage();
+const releaseStatsPage = new ReleaseStatsPage();
 
-describe("QA Stat", () => {
+describe("Release Stats Test", () => {
 
-    it(`Launch browser at ${startPage.urlBrowser + + '/qa/'}`, () => {
-        startPage.visitQaStatPage();
-        cy.wait(2000);
+    it(`Launch Release Stats at ${startPage.urlBrowserReleaseStats}`, () => {
+        startPage.visit();
+        //cy.wait(2000);
     })
 
     Cypress.$.each(['International Edition', 'Austrian Edition', 'Australian Edition',
@@ -19,17 +18,17 @@ describe("QA Stat", () => {
                     'Swedish Edition', 'United States Edition'], (index, value) => {
 
         it(`Select ` + value, () => {
-            qaStatPage.selectEditionByName(value);
+            releaseStatsPage.selectEditionByName(value);
         })
 
         it(`Select and verify DESCRIPTIVE STATISTICS tab in the ` + value, () => {
-            qaStatPage.selectTabByName('DESCRIPTIVE STATISTICS');
+            releaseStatsPage.selectTabByName('DESCRIPTIVE STATISTICS');
             cy.get('.primary-charts').first().should('be.visible');
             cy.get('.secondary-charts ').first().should('be.visible');
         })
 
         it(`Select and verify GENERAL RELEASE STATISTICS tab in the ` + value, () => {
-            qaStatPage.selectTabByName('GENERAL RELEASE STATISTICS');
+            releaseStatsPage.selectTabByName('GENERAL RELEASE STATISTICS');
             cy.get('.general-release-statistics table').first().should('be.visible').within(() => {
                 cy.get('thead > tr').children('th').should('have.length', 5);
                 cy.get('tbody').children('tr').should('have.length.at.least', 1);
@@ -37,7 +36,7 @@ describe("QA Stat", () => {
         })
 
         it(`Select and verify NEW CONCEPTS tab in the ` + value, () => {
-            qaStatPage.selectTabByName('NEW CONCEPTS');
+            releaseStatsPage.selectTabByName('NEW CONCEPTS');
             cy.get('.new-concepts table').first().should('be.visible').within(() => {
                 cy.get('thead > tr').children('th').should('have.length', 5);
                 cy.get('tbody').children('tr').should('have.length.at.least', 1);
@@ -45,7 +44,7 @@ describe("QA Stat", () => {
         })
 
         it(`Select and verify INACTIVATED CONCEPTS tab in the ` + value, () => {
-            qaStatPage.selectTabByName('INACTIVATED CONCEPTS');
+            releaseStatsPage.selectTabByName('INACTIVATED CONCEPTS');
             cy.get('.inactivated-concepts table').first().should('be.visible').within(() => {
                 cy.get('thead > tr').children('th').should('have.length', 3);
                 cy.get('tbody').children('tr').should('have.length.at.least', 1);
@@ -53,7 +52,7 @@ describe("QA Stat", () => {
         })
 
         it(`Select and verify CHANGES TO CONCEPTS tab in the ` + value, () => {
-            qaStatPage.selectTabByName('CHANGES TO CONCEPTS');
+            releaseStatsPage.selectTabByName('CHANGES TO CONCEPTS');
             cy.get('.concept-changes-counts table').first().should('be.visible').within(() => {
                 cy.get('thead > tr').children('th').should('have.length', 5);
                 cy.get('tbody').children('tr').should('have.length.at.least', 1);
@@ -61,7 +60,7 @@ describe("QA Stat", () => {
         })
 
         it(`Select and verify RELEASE SUMMARY tab in the ` + value, () => {
-            qaStatPage.selectTabByName('RELEASE SUMMARY');
+            releaseStatsPage.selectTabByName('RELEASE SUMMARY');
             cy.get('.release-summary table').first().should('be.visible').within(() => {
                 cy.get('thead > tr').children('th').should('have.length', 34);
                 cy.get('tbody').children('tr').should('have.length.at.least', 1);
